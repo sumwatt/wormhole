924268a55da8fc974447ef7e006aa1cd238db9a3
colson <colson@cogfactory.net>
2021-11-12 22:02:38 -0800

    back out carbon

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,42 @@
 <script>
+	import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    Dropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem
+  } from 'sveltestrap';
+
+  let isOpen = false;
+
+  function handleUpdate(event) {
+    isOpen = event.detail.isOpen;
+  }
+
+
 	export let name;
 </script>
 
+
+<svelte:head>
+	<link rel='stylesheet' href='css/bootstrap.min.css'>
+	<link rel='stylesheet' href='css/global.css'>
+</svelte:head>	
+
+
 <main>
-	<h1>Hello {name}!</h1>
+	<h1>Hi {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
 <style>
+	
 	main {
 		text-align: center;
 		padding: 1em;
