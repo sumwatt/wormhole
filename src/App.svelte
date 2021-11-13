3922d0916d31245e8a513cd1c2e44c932c4f27b2
colson <colson@cogfactory.net>
2021-11-12 23:17:44 -0800

    basic bootstrap integration

--- src/App.svelte
+++ src/App.svelte
@@ -1,36 +1,48 @@
 <script>
-	import {
-    Collapse,
-    Navbar,
-    NavbarToggler,
-    NavbarBrand,
-    Nav,
-    NavItem,
-    NavLink,
-    Dropdown,
-    DropdownToggle,
-    DropdownMenu,
-    DropdownItem
-  } from 'sveltestrap';
-
-  let isOpen = false;
-
-  function handleUpdate(event) {
-    isOpen = event.detail.isOpen;
-  }
-
-
+	import 'bootstrap';
 	export let name;
 </script>
 
 
-<svelte:head>
-	<link rel='stylesheet' href='css/bootstrap.min.css'>
-	<link rel='stylesheet' href='css/global.css'>
-</svelte:head>	
-
-
 <main>
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+		<div class="container-fluid">
+			<a class="navbar-brand keychainify-checked" href={'#'}>Navbar</a>
+			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+	
+			<div class="collapse navbar-collapse" id="navbarColor02">
+				<ul class="navbar-nav me-auto">
+					<li class="nav-item">
+						<a class="nav-link active keychainify-checked" href={'#'}>Home
+							<span class="visually-hidden">(current)</span>
+						</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link keychainify-checked" href={'#'}>Features</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link keychainify-checked" href={'#'}>Pricing</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link keychainify-checked" href={'#'}>About</a>
+					</li>
+					<li class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle keychainify-checked" data-bs-toggle="dropdown" href={'#'} role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+						<div class="dropdown-menu">
+							<a class="dropdown-item keychainify-checked" href={'#'}>Action</a>
+							<a class="dropdown-item keychainify-checked" href={'#'}>Another action</a>
+							<a class="dropdown-item keychainify-checked" href={'#'}>Something else here</a>
+							<div class="dropdown-divider"></div>
+							<a class="dropdown-item keychainify-checked" href={'#'}>Separated link</a>
+						</div>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+
 	<h1>Hi {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
