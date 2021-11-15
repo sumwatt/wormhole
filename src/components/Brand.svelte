b2dbc8d79295ff797b43a5b3b73db7946d148021
colson <colson@cogfactory.net>
2021-11-14 18:47:18 -0800

    clean up base ui

---
+++ src/components/Brand.svelte
@@ -0,0 +1,12 @@
+<script>
+
+</script>
+
+<li class="nav-item">
+  <a href="/" class="nav-link d-flex align-items-center">
+    <span class="sidebar-icon">
+      <img src="img/brand/light.svg" height="20" width="20" alt="Volt Logo">
+    </span>
+    <span class="mt-1 ms-1 sidebar-text">Wormhole</span>
+  </a>
+</li>
